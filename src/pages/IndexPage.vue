9d976d4810ee6325a43a23bbfeb67342d250b6bb
Iss best <joel.sfyritech@pm.me>
2024-08-17 17:15:38 +0300

    login

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="row justify-center items-center">
+    <q-card>
+      <q-card-section class="row q-py-xl">
+        <div class="col-12 text-h6 text-center">Connection</div>
+        <q-input filled v-model="phone" label="N°Téléphone" class="col-12 q-my-md" />
+        <q-input filled v-model="password" label="Password" type="password" class="col-12 q-my-md" />
+        <q-btn label="Login" @click="login" class="col-12 q-mt-md" color="primary" />
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+<script setup>
+import { ref } from 'vue'
 
-defineOptions({
-  name: 'IndexPage'
-});
+const username = ref('')
+const password = ref('')
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const login = () => {
+  console.log('Username:', username.value)
+  console.log('Password:', password.value)
+}
 </script>
+
+<style scoped>
+.q-card {
+  width: 300px;
+}
+</style>
